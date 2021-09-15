3e78563e7e920017e29f263e4026e5afa12c1ff9
Bhuvana-295556 <82052062+Bhuvana-295556@users.noreply.github.com>
2021-09-15 08:04:35 +0530

    Update Stored Procedure.sql

--- Stored Procedure.sql
+++ Stored Procedure.sql
@@ -10,3 +10,14 @@ ALTER TABLE [user]
   ADD PRIMARY KEY ([ID]);
 
  SELECT *FROM [user];
+ 
+ --Delaete Operation--
+ 
+ DELETE FROM [user] 
+ WHERE  ID=10;
+SELECT *FROM [user];
+
+--Update operation--
+
+UPDATE [user] SET [FullName]='Ram',[Email]='ram23@gmail.com' WHERE [ID]=8;
+SELECT *FROM [user]
